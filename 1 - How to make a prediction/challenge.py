b5fe9802373a894ce851dd6843cece677a8dfe06
adrielvieira <adrielmvieira@gmail.com>
2017-01-18 10:38:35 -0200

    challenge 1 solution

---
+++ 1 - How to make a prediction/challenge.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('1 - How to Make a Prediction/challenge_dataset.txt', header = None)
+df.columns = ['x','y']
+x = df[[0]]
+y = df[[1]]
+
+reg = linear_model.LinearRegression()
+reg.fit(x,y)
+
+plt.scatter(x,y)
+plt.plot(x,reg.predict(x))
+plt.show()
+
+x_test = df['x'][0]
+y_test = df['y'][0]
+y_predicted = reg.predict(x_test)[0][0]
+
+print 'y = ', y_test
+print 'y predicted = ', y_predicted
+print 'error = ', y_predicted - y_test
